585e1e432ff046d90aace4a1c29eb101a21a3c3e
GuiceJoose <snowmanjz@gmail.com>
2022-12-21 21:58:56 -0600

    Build layout component

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Clamazon" : "Clamazon"}</title>
+        <meta
+          name="description"
+          content="Oder the finest saltwater invertebrates and corals straight to your door."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between">
+            <Link className="text-lg p-2" href="/">
+              Clamazon
+            </Link>
+            <div>
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center ">
+          <p>Copyright© 2023 Clamazon</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
+import Layout from "../components/Layout";
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Clamazon</title>
-        <meta
-          name="description"
-          content="Oder the finest saltwater invertebrates and corals straight to your door."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Layout title="Home">Home</Layout>
     </>
   );
 }
